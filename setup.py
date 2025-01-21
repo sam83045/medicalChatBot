09de52aca4b95409e2d20b6ce912b5f3d2ac41f6
shoaib <shoaib786k@gmail.com>
2025-01-21 20:36:50 +0530

    feat: folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="Generative AI project",
+    version="0.0.0",
+    author="Shoaib Konnur",
+    author_email="shoaib786k@gmail.com",
+    packages=find_packages(),    
+)
